a0a5f80e90a1a8e961fae32f6448aee2b2b283cf
Aashay-B <aashay@cioffisgroup.org>
2025-06-12 13:48:19 -0700

    fix bugs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
 import TempRecorderForm from './components/TempRecorderForm';
